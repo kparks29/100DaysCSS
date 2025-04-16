bf1faf512db99b5db2de55f7ed8991cd2ae83bf2
Kendrick Parks <parks.kendrick@gmail.com>
2025-04-16 13:15:17 -0700

    making sure build works

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { sync } from 'glob'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: sync("./src/**/*.html".replace(/\\/g, "/")),
+    }
+  }
 })
